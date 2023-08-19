6c9486e6e6b183c853d9f58c331b41767b389295
laurence0202 <laurence0202@users.noreply.github.com>
2023-08-19 18:07:16 +1000

    added more redux state functions for cart total, quantity

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,31 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    cart: [
-        {
-            pizzaId: 12,
-            name: "Pizza 12",
-            quantity: 2,
-            unitPrice: 16,
-            totalPrice: 32,
-        },
-    ],
+    cart: [],
+//   cart: [
+//     {
+//       pizzaId: 12,
+//       name: 'Pizza 12',
+//       quantity: 2,
+//       unitPrice: 16,
+//       totalPrice: 32,
+//     },
+//   ],
 };
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addItem: (state, action)=> {
-        },
-        deleteItem: (state, action)=> {
-        },
-        increaseItemQuantity: (state, action)=> {
-        },
-        decreaseItemQuantity: (state, action)=> {
-        },
-        clearCart: (state, action)=> {
-        }
-    }
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    deleteItem: (state, action) => {
+      state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity: (state, action) => {
+        const item = state.cart.find((item) => item.pizzaId === action.payload);
+        item.quantity++;
+        item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity: (state, action) => {
+        const item = state.cart.find((item) => item.pizzaId === action.payload);
+        item.quantity--;
+        item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart: (state) => {
+        state.cart = [];
+    },
+  },
 });
-export const { updateName  } = cartSlice.actions;
+
+export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export const getTotalCartPrice = (state) => state.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0);
+export const getTotalCartQuantity = (state) => state.cart.cart.reduce((acc, item) => acc + item.quantity, 0)
+  
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,25 @@
 import { formatCurrency } from '../../utils/helpers';
 import Button from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleAddtoCart = () => {
+    // console.log(" in handleAdd before adding MenuItem, cart is: ", cart);
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+    console.log(" in handleAdd after MenuItem, cart is: ", cart);
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -25,7 +42,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to Cart</Button>
+          {!soldOut ? (
+            <Button type="small" onClick={handleAddtoCart}>
+              Add to Cart
+            </Button>
+          ) : null}
         </div>
       </div>
     </li>
